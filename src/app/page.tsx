05ddb55e2feea108c956a9134cb2e42b86823321
abnody <ahmedalabnody45@gmail.com>
2025-07-14 01:05:14 +0300

    steps & formData handle

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,76 @@
-import Image from "next/image";
-import {Button} from "@/components/ui/button";
+"use client";
+import { z } from "zod";
+import { useState } from "react";
+import { Button } from "@/components/ui/button";
+import type { Field } from "../app/interface";
+import CrudForm from "@/components/crud-form";
+
 export default function Home() {
+  const fields: Field[] = [
+    { name: "email", type: "email", label: "Email", defaultValue:"abcd@gmail.com" ,step:1},
+    { name: "password", type: "password", label: "Password",defaultValue:"01127769663", step:1},
+    {
+      name: "gender",
+      type: "select",
+      placeholder: "Select Gender",
+      defaultValue:"female",
+      options: [
+        { value: "male", placeholder: "Male" },
+        { value: "female", placeholder: "Female" },
+      ],step:2
+    },
+    { name: "dob", type: "date", placeholder: "Choose birth date", defaultValue:"2000-01-01",step:2 },
+    { name: "file", type: "file", placeholder: "Choose file date",step:3},
+    { name: "terms", type: "checkbox", label: "Accept Terms", defaultValue: true, step:3 },
+  ];
+
+  const steps=[1,2,3]
+
+  const validationSchema = z.object({
+    email: z.string().email("Enter a valid Email"),
+    password: z.string().min(10, "Password must be at least 10 characters"),
+    gender: z.string(),
+    dob: z.string(),
+    terms: z.boolean(),
+    file: z.any()
+  })
+
+
+  const [isOpen, setIsOpen] = useState(false);
+  const [operation, setOperation] = useState<"add" | "edit" | "preview" >("add");
+
   return (
-   <div>
+    <div className="p-6 space-y-4">
 
-     <h1 className={"text-3xl"}>testing</h1>
+      <div className="flex flex-wrap gap-2">
+        <Button onClick={() => {
+          setOperation("add");
+          setIsOpen(true);
+        }}>Add</Button>
 
-       <Button className={"bg-primary text-accent"}>test</Button>
-   </div>
+        <Button onClick={() => {
+          setOperation("edit");
+          setIsOpen(true);
+        }}>Edit</Button>
+
+        <Button onClick={() => {
+          setOperation("preview");
+          setIsOpen(true);
+        }}>Review</Button>
+
+      </div>
+
+      {isOpen && (
+        <CrudForm
+          fields={fields}
+          isOpen={isOpen}
+          setIsOpen={setIsOpen}
+          operation={operation}
+          asDialog={true}
+          validationSchema={validationSchema}
+          steps={steps}
+        />
+      )}
+    </div>
   );
 }
