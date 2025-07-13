e91f40b19bf46dd213d47ba946ea67ac07d3f1f6
shahd-abdo2711 <shahdhussinabdo@gmail.com>
2025-07-13 23:00:01 +0300

    support multiple form operations (add, edit, review)

---
+++ src/app/interface.tsx
@@ -0,0 +1,14 @@
+export interface FieldOption {
+  value: string;
+  placeholder: string;
+}
+
+export interface Field {
+  name: string;
+  type: string;
+  label?: string;
+  placeholder?: string;
+  defaultValue?: string|boolean;
+  value?: string;
+  options?: FieldOption[];
+}
