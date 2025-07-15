2d2f85c1de5c1f44dc2df4ab997f1f0e7d37a232
Ali-abdelrady <aliabdelrady2k16@gmail.com>
2025-07-15 15:46:13 +0300

    a

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,24 +7,49 @@ import CrudForm from "@/components/crud-form";
 
 export default function Home() {
   const fields: Field[] = [
-    { name: "email", type: "email", label: "Email", defaultValue:"abcd@gmail.com" ,step:1},
-    { name: "password", type: "password", label: "Password",defaultValue:"01127769663", step:1},
+    {
+      name: "email",
+      type: "email",
+      label: "Email",
+      defaultValue: "abcd@gmail.com",
+      step: 1,
+    },
+    {
+      name: "password",
+      type: "password",
+      label: "Password",
+      defaultValue: "01127769663",
+      step: 1,
+    },
     {
       name: "gender",
       type: "select",
       placeholder: "Select Gender",
-      defaultValue:"female",
+      defaultValue: "female",
       options: [
         { value: "male", placeholder: "Male" },
         { value: "female", placeholder: "Female" },
-      ],step:2
+      ],
+      step: 2,
     },
-    { name: "dob", type: "date", placeholder: "Choose birth date", defaultValue:"2000-01-01",step:2 },
-    { name: "file", type: "file", placeholder: "Choose file date",step:3},
-    { name: "terms", type: "checkbox", label: "Accept Terms", defaultValue: true, step:3 },
+    {
+      name: "dob",
+      type: "date",
+      placeholder: "Choose birth date",
+      defaultValue: "2000-01-01",
+      step: 2,
+    },
+    { name: "file", type: "file", placeholder: "Choose file date", step: 3 },
+    {
+      name: "terms",
+      type: "checkbox",
+      label: "Accept Terms",
+      defaultValue: true,
+      step: 3,
+    },
   ];
 
-  const steps=[1,2,3]
+  const steps = [1, 2, 3];
 
   const validationSchema = z.object({
     email: z.string().email("Enter a valid Email"),
@@ -32,32 +57,41 @@ export default function Home() {
     gender: z.string(),
     dob: z.string(),
     terms: z.boolean(),
-    file: z.any()
-  })
-
+    file: z.any(),
+  });
 
   const [isOpen, setIsOpen] = useState(false);
-  const [operation, setOperation] = useState<"add" | "edit" | "preview" >("add");
+  const [operation, setOperation] = useState<"add" | "edit" | "preview">("add");
 
   return (
     <div className="p-6 space-y-4">
-
       <div className="flex flex-wrap gap-2">
-        <Button onClick={() => {
-          setOperation("add");
-          setIsOpen(true);
-        }}>Add</Button>
+        <Button
+          onClick={() => {
+            setOperation("add");
+            setIsOpen(true);
+          }}
+        >
+          Add
+        </Button>
 
-        <Button onClick={() => {
-          setOperation("edit");
-          setIsOpen(true);
-        }}>Edit</Button>
+        <Button
+          onClick={() => {
+            setOperation("edit");
+            setIsOpen(true);
+          }}
+        >
+          Edit
+        </Button>
 
-        <Button onClick={() => {
-          setOperation("preview");
-          setIsOpen(true);
-        }}>Review</Button>
-
+        <Button
+          onClick={() => {
+            setOperation("preview");
+            setIsOpen(true);
+          }}
+        >
+          Preview
+        </Button>
       </div>
 
       {isOpen && (
