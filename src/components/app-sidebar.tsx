de8cbcb50208f0fa534ad6ae5e4f6160ff70ed57
Ali-abdelrady <aliabdelrady2k16@gmail.com>
2025-08-02 18:19:24 +0300

    fix error

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -1,160 +1,75 @@
-"use client"
+"use client";
 
-import * as React from "react"
+import * as React from "react";
 import {
   AudioWaveform,
   BookOpen,
   Bot,
+  Building,
   Command,
   Frame,
   GalleryVerticalEnd,
+  HomeIcon,
   Map,
   PieChart,
   Settings2,
   SquareTerminal,
-} from "lucide-react"
+} from "lucide-react";
 
-import { NavMain } from "@/components/nav-main"
-import { NavProjects } from "@/components/nav-projects"
-import { NavUser } from "@/components/nav-user"
-import { TeamSwitcher } from "@/components/team-switcher"
+import { NavMain } from "@/components/nav-main";
+import { NavProjects } from "@/components/nav-projects";
+import { NavUser } from "@/components/nav-user";
+import { TeamSwitcher } from "@/components/team-switcher";
 import {
   Sidebar,
   SidebarContent,
   SidebarFooter,
   SidebarHeader,
   SidebarRail,
-} from "@/components/ui/sidebar"
+} from "@/components/ui/sidebar";
 
 // This is sample data.
 const data = {
-  user: {
-    name: "shadcn",
-    email: "m@example.com",
-    avatar: "/avatars/shadcn.jpg",
-  },
   teams: [
     {
       name: "Acme Inc",
       logo: GalleryVerticalEnd,
       plan: "Enterprise",
     },
-    {
-      name: "Acme Corp.",
-      logo: AudioWaveform,
-      plan: "Startup",
-    },
-    {
-      name: "Evil Corp.",
-      logo: Command,
-      plan: "Free",
-    },
   ],
   navMain: [
     {
-      title: "Playground",
-      url: "#",
-      icon: SquareTerminal,
+      title: "Dashboard",
+      url: "/",
+      icon: HomeIcon,
+      isActive: true,
+    },
+    {
+      title: "Bootcamp Management",
+      url: "/bootcamp",
+      icon: Building,
       isActive: true,
       items: [
         {
-          title: "History",
-          url: "#",
+          title: "Analytics",
+          url: "/bootcamp/analytics",
         },
         {
-          title: "Starred",
-          url: "#",
+          title: "Workshops",
+          url: "/bootcamp/workshops",
         },
         {
-          title: "Settings",
-          url: "#",
-        },
-      ],
-    },
-    {
-      title: "Models",
-      url: "#",
-      icon: Bot,
-      items: [
-        {
-          title: "Genesis",
-          url: "#",
+          title: "Participants",
+          url: "/bootcamp/participants",
         },
         {
-          title: "Explorer",
-          url: "#",
-        },
-        {
-          title: "Quantum",
-          url: "#",
-        },
-      ],
-    },
-    {
-      title: "Documentation",
-      url: "#",
-      icon: BookOpen,
-      items: [
-        {
-          title: "Introduction",
-          url: "#",
-        },
-        {
-          title: "Get Started",
-          url: "#",
-        },
-        {
-          title: "Tutorials",
-          url: "#",
-        },
-        {
-          title: "Changelog",
-          url: "#",
-        },
-      ],
-    },
-    {
-      title: "Settings",
-      url: "#",
-      icon: Settings2,
-      items: [
-        {
-          title: "General",
-          url: "#",
-        },
-        {
-          title: "Team",
-          url: "#",
-        },
-        {
-          title: "Billing",
-          url: "#",
-        },
-        {
-          title: "Limits",
-          url: "#",
+          title: "Email Tracking",
+          url: "/bootcamp/emails",
         },
       ],
     },
   ],
-  projects: [
-    {
-      name: "Design Engineering",
-      url: "#",
-      icon: Frame,
-    },
-    {
-      name: "Sales & Marketing",
-      url: "#",
-      icon: PieChart,
-    },
-    {
-      name: "Travel",
-      url: "#",
-      icon: Map,
-    },
-  ],
-}
+};
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
   return (
@@ -164,12 +79,10 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
       </SidebarHeader>
       <SidebarContent>
         <NavMain items={data.navMain} />
-        <NavProjects projects={data.projects} />
+        {/* <NavProjects projects={data.projects} /> */}
       </SidebarContent>
-      <SidebarFooter>
-        <NavUser user={data.user} />
-      </SidebarFooter>
+      <SidebarFooter>{/* <NavUser user={data.user} /> */}</SidebarFooter>
       <SidebarRail />
     </Sidebar>
-  )
+  );
 }
--- src/components/cards/CardsGrid.tsx
+++ src/components/cards/CardsGrid.tsx
@@ -1,5 +1,5 @@
 import InfoCard from "./InfoCard";
-import type { CardData } from "@/app/dashboard/page";
+import type { CardData } from "@/app/(dashboard)/dashboard/page";
 
 type CardsGridProps = {
   data: CardData[];
@@ -8,19 +8,17 @@ type CardsGridProps = {
 export default function CardsGrid({ data }: CardsGridProps) {
   return (
     <>
-       
       <div className="grid w-full grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-4 mb-4">
         {data
-          .filter((item) => !item.type)  
+          .filter((item) => !item.type)
           .map((item, index) => (
             <InfoCard key={index} {...item} />
           ))}
       </div>
 
-       
       <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-2 gap-4 w-full">
         {data
-          .filter((item) => item.type)   
+          .filter((item) => item.type)
           .map((item, index) => (
             <div key={index} className="w-full h-full">
               <InfoCard {...item} />
@@ -30,13 +28,3 @@ export default function CardsGrid({ data }: CardsGridProps) {
     </>
   );
 }
-
-
-
-
-
-
-
-
-
-
