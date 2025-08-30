d00f8158940a6ee37ad9b80688d482d067cee71e
DoaaAhmed91 <dodyahmed912006@gmail.com>
2025-08-30 19:41:11 +0300

    login ui

--- src/app/(Auth)/login/page.tsx
+++ src/app/(Auth)/login/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { GalleryVerticalEnd } from "lucide-react";
 import { LoginForm } from "../../../components/login-form";
 
 
@@ -8,15 +7,10 @@ export default function LoginPage() {
 return (
     <div
       className="flex min-h-screen items-center justify-center bg-cover bg-center"
-      style={{ backgroundImage: "url('/')" }} 
+      style={{ backgroundImage: "url('/images/nasalogin.jpg')" }} 
     >
       <div className="flex w-full max-w-sm flex-col gap-6 bg-transparent p-6 rounded-xl border border-white/30">
-        <a href="#" className="flex items-center gap-2 self-center font-medium text-white">
-          <div className="bg-primary text-primary-foreground flex size-6 items-center justify-center rounded-md">
-            <GalleryVerticalEnd className="size-4" />
-          </div>
-          Acme Inc.
-        </a>
+
         <LoginForm />
       </div>
     </div>
