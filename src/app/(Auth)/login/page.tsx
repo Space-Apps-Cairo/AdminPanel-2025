7c51a4003fe32e9ef91921f76f7531881c471fbe
Youssef Elaraby <y.ahmed.ea@gmail.com>
2025-09-07 20:11:30 +0300

    fix login ui

--- src/app/(Auth)/login/page.tsx
+++ src/app/(Auth)/login/page.tsx
@@ -4,12 +4,12 @@ import { LoginForm } from "../../../components/login-form";
 
 
 export default function LoginPage() {
-return (
+  return (
     <div
       className="flex min-h-screen items-center justify-center bg-cover bg-center"
-      style={{ backgroundImage: "url('/images/nasalogin.jpg')" }} 
+      style={{ backgroundImage: "url('/images/nasalogin.jpg')" }}
     >
-      <div className="flex w-full max-w-sm flex-col gap-6 bg-transparent p-6 rounded-xl border border-white/30">
+      <div className="flex w-full max-w-sm flex-col gap-6 bg-white dark:bg-black p-6 rounded-xl border border-white/30">
 
         <LoginForm />
       </div>
